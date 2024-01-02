53de0035197c273a4db42c96ee1044182a071f7c
Denis <denis.tolmachev@gmail.com>
2024-01-02 17:59:33 +0200

    added img on About page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import styles from "./about.module.css";
 
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
-  )
-}
+    <div>
+      <div className={styles.imgContainer}>
+        <Image src="/about.png" alt="" fill />
+      </div>
+    </div>
+  );
+};
 
-export default AboutPage
+export default AboutPage;
