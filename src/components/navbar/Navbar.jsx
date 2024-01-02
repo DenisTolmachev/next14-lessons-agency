a5bfb2254291122f64b959d07a6d6924c956a47a
Denis <denis.tolmachev@gmail.com>
2024-01-02 13:36:03 +0200

    added css from navBar links

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,11 @@
 import Links from "./links/Links";
 import React from "react";
+import styles from "./navbar.module.css";
 
 const Navbar = () => {
   return (
-    <div>
-      <div>Logo</div>
+    <div className={styles.container}>
+      <div className={styles.logo}>Denny`s Creative Agency</div>
       <div>
         <Links />
       </div>
---
+++ src/components/navbar/links/navLink/navLink.jsx
@@ -0,0 +1,25 @@
+"use client";
+
+import React from "react";
+import Link from "next/link";
+import styles from "./navLink.module.css";
+import { usePathname } from "next/navigation";
+
+const NavLink = ({ item }) => {
+  const pathName = usePathname();
+
+  return (
+    <div>
+      <Link
+        className={`${styles.container} ${
+          pathName === item.path && styles.active
+        }`}
+        href={item.path}
+      >
+        {item.title}
+      </Link>
+    </div>
+  );
+};
+
+export default NavLink;
