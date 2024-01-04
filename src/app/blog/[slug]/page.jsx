f84cb1dd11be5f931c7d6913879bfe9943a9ab71
Denis <denis.tolmachev@gmail.com>
2024-01-04 19:31:31 +0200

    singlePost page disign and responsive components

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
+import styles from "./singlePost.module.css";
+import Image from "next/image";
 
 const SinglePostPage = () => {
   return (
-    <div>SinglePostPage</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image
+          className={styles.img}
+          src="https://images.pexels.com/photos/13182836/pexels-photo-13182836.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt=""
+          fill
+        />
+      </div>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Title</h1>
+        <div className={styles.detail}>
+          <Image
+            className={styles.avatar}
+            src="https://images.pexels.com/photos/13182836/pexels-photo-13182836.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            width={50}
+            height={50}
+          />
+          <div className={styles.detailText}>
+            <div className={styles.detailTitle}>Author</div>
+            <div className={styles.detailValue}>Denys Tolmachov</div>
+          </div>
+          <div className={styles.detailText}>
+            <div className={styles.detailTitle}>Published</div>
+            <div className={styles.detailValue}>01.01.2024</div>
+          </div>
+        </div>
+        <div className={styles.content}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+          officia in vero, quae, nesciunt sapiente odio pariatur est inventore,
+          voluptatum maxime expedita repellendus repudiandae laborum velit
+          libero? Laboriosam, harum pariatur.
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default SinglePostPage
+export default SinglePostPage;
